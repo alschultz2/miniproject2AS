6f80a60fa296869fb6339c64f3454e9c19eccb50
alschultz2 <alschultz2@mail.fhsu.edu>
2023-09-24 21:13:22 -0500

    Updated the ReadMe file and uploaded the data for the amount of land for countries. The information is stored using a pandas dataframe.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# miniproject2AS
+# miniproject1AS
+
+INF601 - Advanced Programming with Python
+
+Adam Schultz
+
+miniproject2AS
+
+## Description
+This project will be using Pandas and Faker to create dataframes.
+This isn't intended to be full blown data science project.
+The goal here is to come up with some question and then see what API or
+datasets you can use to get the information needed to answer that question.
+
+## Pip install instructions
+In order to install all of the files necessary for the program,
+please run the following:
+```
+pip install -r requirements.txt
+```
+By running this line you should  be able to execute the program.
+
+## How to run
+In a terminal window, please type the following:
+```
+python main.py
+```
+This will run the main python program. 
--- main.py
+++ main.py
@@ -3,13 +3,22 @@
 # Mini Project #2
 
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file. #Done
-# (5/5 points) Proper import of packages used.
+
+# (5/5 points) Proper import of packages used. #Done
+
+import pandas as pd
+
+
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
         # Think of some question you would like to solve such as:
         # "How many homes in the US have access to 100Mbps Internet or more?"
         # "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
         # Here are some other great datasets: https://www.kaggle.com/datasets
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
+
+data = pd.read_csv('All Countries.csv', index_col='land_area')
+print(data.head())
+
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
